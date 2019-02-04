76171c6b04f3351b5db8c8f0a33d73037d1832c7
MailsonD <mailssondennis@gmail.com>
2019-02-04 08:55:16 -0300

    Finalizando testes para Cliente

--- src/test/java/ClienteTest.java
+++ src/test/java/ClienteTest.java
@@ -1,30 +1,58 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.logging.Logger;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import com.ifpb.biblioteca.exceptions.DadosInvalidosException;
 import com.ifpb.biblioteca.model.Dao.ClienteDAO;
 import com.ifpb.biblioteca.model.Dao.ClienteDaoImpl;
+import com.ifpb.biblioteca.model.Dao.EmprestimoDAO;
 import com.ifpb.biblioteca.model.Entities.Cliente;
+import com.ifpb.biblioteca.model.Entities.Emprestimo;
 
 public class ClienteTest {
 	
 	private ClienteDAO clienteDao;
 	
+	@Mock
+	private EmprestimoDAO emprestimoDao;
+	
 	@Before
-	public void iniciarTestes() {
+	public void iniciarTestes() throws DadosInvalidosException {
+		MockitoAnnotations.initMocks(this);
 		clienteDao = new ClienteDaoImpl();
+		clienteDao.cadastrar("333.333.333-03", "Mailson", LocalDate.now(), "mailsuu@gmail.com" , "456123");
 	}
+	
 	@Test
 	public void cadastrarCliente() {
 		try {
 			Assert.assertTrue(clienteDao.cadastrar("111.111.111-01", "Lucas", LocalDate.now(), "luquinhasEvangelista@gmail.com" , "456123"));
 		} catch (DadosInvalidosException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
-
+	
+	@Test
+	public void cadastrarClienteSemEmail() {
+		try {
+			Assert.assertFalse(clienteDao.cadastrar("111.111.111-01", "Lucas", LocalDate.now(), null , "456123"));
+		} catch (DadosInvalidosException e) {
+			// TODO Auto-generated catch block
+		}
+	}
+	
+	@Test
+	public void buscarPendenciasSemEmprestimos() {
+		Mockito.when(emprestimoDao.listarEmprestimos()).thenReturn(new ArrayList<Emprestimo>());
+		Assert.assertEquals(new ArrayList<String>(), clienteDao.buscarPendencias("mailsuu@gmail.com",emprestimoDao));
+	}
+	
+	
 }
