9e09f7732e4ecf349fbfac78e2497b558c401ce0
antonio <antoniofilhonerde@gmail.com>
2019-02-04 15:14:58 -0300

    Criação da Suite de Testes

---
+++ src/test/java/test/Testes.java
@@ -0,0 +1,19 @@
+package test;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+        ClienteTest.class,
+        EmprestimoTest.class,
+        FuncionarioTest.class,
+        LivroTest.class,
+        ReservaTest.class
+})
+
+public class Testes {
+
+}
