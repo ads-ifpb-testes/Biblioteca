bc765098861e783e0f175e5c741c9164a4041965
camiula <camilacarwalho3@gmail.com>
2019-03-11 16:20:27 -0300

    Último teste feito

--- src/test/java/test/testesdeintegacao/EmprestimoTestI.java
+++ src/test/java/test/testesdeintegacao/EmprestimoTestI.java
@@ -4,12 +4,10 @@ package test.testesdeintegacao;
 import com.ifpb.biblioteca.exceptions.LivroIndisponivelException;
 
 import com.ifpb.biblioteca.exceptions.LivroInvalidoException;
-import com.ifpb.biblioteca.model.Dao.ClienteDAO;
-import com.ifpb.biblioteca.model.Dao.EmprestimoDAO;
-import com.ifpb.biblioteca.model.Dao.EmprestimoDaoImpl;
-import com.ifpb.biblioteca.model.Dao.LivroDAO;
+import com.ifpb.biblioteca.model.Dao.*;
 import com.ifpb.biblioteca.model.Entities.Cliente;
 import com.ifpb.biblioteca.model.Entities.Emprestimo;
+import com.ifpb.biblioteca.model.Entities.Reserva;
 import com.ifpb.biblioteca.model.Enum.LivroEnum;
 import org.junit.Assert;
 import org.junit.Before;
@@ -31,7 +29,8 @@ public class EmprestimoTestI {
     private ClienteDAO clienteDao;
     @Mock
     private LivroDAO livroDAO;
-
+    @Mock
+    private ReservaDAO reservaDAO;
 
     private EmprestimoDAO emprestimoDAO = new EmprestimoDaoImpl();
     private String email = "tonyy@gmail.com";
@@ -39,6 +38,8 @@ public class EmprestimoTestI {
             LocalDate.now(), email, "321123");
     private Livro livro = new Livro("Harry Potter", 3, "Roco", "J.K.Rolling", LivroEnum.CONSULTA);
     private Livro livro2 = new Livro("Angeologia",7,"Fonte editorial","Marcelo",LivroEnum.EMPRESTIMO);
+    private Livro livro3 = new Livro("Nome do vento",8,"Random","Patrick", LivroEnum.EMPRESTIMO);
+    private Reserva reserva = new Reserva(livro3,cliente,LocalDate.now());
 
 
     @Before
@@ -63,9 +64,16 @@ public class EmprestimoTestI {
     public void emprestarLivroIndisponivel(){
         try {
             Assert.assertFalse(emprestimoDAO.emprestar(livro2,"mailsuu@gmail.com",clienteDao));
-        } catch (LivroIndisponivelException e) {
+        } catch (LivroIndisponivelException | LivroInvalidoException e) {
             e.printStackTrace();
-        } catch (LivroInvalidoException e) {
+        }
+    }
+
+    @Test
+    public void renovarEmprestimoLivroReservado(){
+        try {
+            Assert.assertFalse(emprestimoDAO.emprestar(livro3,"tonyy@gmail.com",clienteDao));
+        }catch (LivroIndisponivelException | LivroInvalidoException e){
             e.printStackTrace();
         }
     }
